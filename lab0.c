bfbbe32aa78ab076eaf0955b9c5fd8f52d2501d5
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-09-01 18:28:32 +0000

    Draft lab0 solution

--- lab0.c
+++ lab0.c
@@ -2,6 +2,16 @@
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    int sum = 0;
+    int i = 0;
+
+    while( i < 200 )
+    {
+        sum += i;
+        i += 2;
+    }
+
+    printf("Hello, World!");
+    printf("%d\n", sum);
     return 0;
 }
