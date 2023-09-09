6cabb0e99e3c3416be23d14f7c3825112fde9e60
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-09-09 17:24:18 +0000

    exercise 2

--- happy.c
+++ happy.c
@@ -13,7 +13,18 @@ int main()
 	//add code below
 	
 
-
+	while(n != 1 && n != 4)
+	{
+		int sum = 0;
+		while(n > 0)
+		{
+			int digit = n % 10;
+			n /= 10;
+			sum += digit * digit;
+		}
+		n = sum;
+		printf("%d\n", n);
+	}
 
 
 
