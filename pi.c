297a07862f04282a0d1931e757a976dab13d2d5a
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-09-13 13:56:17 -0400

    Update pi.c for efficiency

--- pi.c
+++ pi.c
@@ -10,18 +10,15 @@ int main()
 
 	double pi = 0.0;
 	//TODO
-	
+	double denominator = 1;
 	for(int i = 0; i <= n; i++)
 	{
 		double internal = (4.0 / (8.0*i + 1.0)) - (2.0/(8.0*i+4.0))
 			- (1.0/(8.0*i+5)) - (1.0/(8.0*i+6.0));
 
-		double denominator = 1;
-		for (int j = 0; j < i; j++){
-			denominator *= 16; 
-		}
 
 		pi += internal/denominator;
+		denominator *= 16;
 	}
 	
 
