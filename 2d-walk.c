6d35abe8296eb0bf1cf7fa8805265543ee5ff7ed
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-09-15 19:19:46 +0000

    :sparkles: add starter files

---
+++ 2d-walk.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double two_d_random(int n)
+{
+
+	//Fill in code below
+	//When deciding which way to go for the next step, generate a random number as follows.
+	//r = rand() % 4;
+	//Treat r = 0, 1, 2, 3 as up, right, down and left respectively.
+
+	//The random walk should stop once the x coordinate or y coordinate reaches $-n$ or $n$. 
+	//The function should return the fraction of the visited $(x, y)$ coordinates inside (not including) the square.
+
+
+
+
+
+	
+}
+
+//Do not change the code below
+int main()
+{
+	int trials = 1000;
+	int i, n, seed;
+	if (argc == 2) seed = atoi(argv[1]);
+	else seed = 12345;
+
+	srand(seed);
+	for(n=1; n<=64; n*=2)
+	{	
+		double sum = 0.;
+		for(i=0; i < trials; i++)
+		{
+			double p = two_d_random(n);
+			sum += p;
+		}
+		printf("%d %.3lf\n", n, sum/trials);
+	}
+	return 0;
+}
+
