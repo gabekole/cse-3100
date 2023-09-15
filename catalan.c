b9a8edd07b673e2ddfde5a796223294d4814f7fe
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-09-15 19:15:00 +0000

    update catalan function

--- catalan.c
+++ catalan.c
@@ -11,7 +11,11 @@ long catalan_number(int n)
     if (n < 0)
         return 0;
 
-    // TODO
+    if (n == 0)
+        return 1;
+
+    int previous = catalan_number(n-1);
+    int factor = (4*n*previous - 2*previous)/(n+1);
 }
 
 /* do not change main function */
