ce9149070d27c477cf54bdecd066eb06a428714d
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-10-06 19:06:53 +0000

    add multiple choice

---
+++ test.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+//     int *p = malloc(sizeof(int));
+//     if(p == NULL)
+//     {
+//         perror("Memory allocation erorr\n");
+//         return -1;
+//     }
+//     if((unsigned long)&p > (unsigned long) p) printf("hello word\n");
+
+//     free(p);
+//     return 0;
+
+
+// int *p;
+// printf("%p\n", p);
+// printf("%d\n", *p);
+// return 0;
+
+// double sum = 0.;
+// for(int i = 1; i <= 1000000; i++)
+//     sum += 1/i/i;
+// printf("sum = %lf\n", sum);
+// return 0;
+
+// int a[] = {1, 2, 3};
+// char *p = a;
+// printf("%d\n", p[0] + p[1] + p[2]);
+// return 0;
+
+// int a[10], *p =a, i;
+
+// for(i = 0; i < 10; i++) *++p = i;
+
+// for(i = 0; i < 10; i++)
+//     printf("%d ", a[i]);
+// printf("\n");
+
+// return 0;
+
+// int a[] = {1};
+// int *p = a;
+// int r = (unsigned long) p == (unsigned long) &p;
+// printf("%d\n", r);
+// return 0;
+
+// int a[] = {1};
+// int *p = a;
+// *p++;
+// printf("%d\n", a[0]);
+
+// int t[10];
+// char * p = (char *) t + 8;
+// char * q = (char *) (t + 8);
+// printf("%ld\n", q-p);
+// return 0;
+
+
+// int a = 1;
+// if(a=2){
+//     printf("a=%d\n", a);
+
+// }
+// return 0;
+
+}
+
