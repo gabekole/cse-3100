5d74793e5b3fdad68d243fc8a02ebf77b0d7c3b4
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-10-06 18:37:18 +0000

    complete stack implementation

--- stack.c
+++ stack.c
@@ -6,23 +6,27 @@
 //TODO:
 void push(stack *s, node *new_node)
 {
-
+    add_first(&(s->top), new_node);
 }
 
 //TODO:
 node* pop(stack *s)
 {
-
+    return remove_first(&(s->top));
 }
 
 //TODO:
 int empty(stack *s)
 {
-
+    return s->top == NULL;
 }
 
 //TODO:
 void clear_stack(stack *s)
 {
-
+    while(!empty(s)) // DO I NEED TO SET NULL CHECK
+    {
+        node * t = remove_first(&(s->top));
+        free(t);
+    }
 }
