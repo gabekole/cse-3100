8ad2c9fee0123826f9983f61608e67b0d94d2f24
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-10-16 15:21:28 +0000

    Add starter files

---
+++ run2.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char ** argv)
+{
+    pid_t child;
+    int exitStatus;
+
+    // at least, there should be 3 arguments
+    // 2 for the first command, and the rest for the second command
+    if (argc < 4) {
+        fprintf(stderr, "Usage: %s cmd1 cmd1_arg cmd2 [cmd2_args ..]\n", argv[0]);
+        return 1;
+    }
+
+    // TODO
+    return 0;
+}
