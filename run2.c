c6ed600bcac81f4ac49cb6883f987e765474f272
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-10-16 16:06:46 +0000

    update prints for failure

--- run2.c
+++ run2.c
@@ -11,11 +11,49 @@ int main(int argc, char ** argv)
 
     // at least, there should be 3 arguments
     // 2 for the first command, and the rest for the second command
+    
     if (argc < 4) {
         fprintf(stderr, "Usage: %s cmd1 cmd1_arg cmd2 [cmd2_args ..]\n", argv[0]);
         return 1;
     }
 
+
+    child = fork();
+    if(child < 0)
+    {
+        perror("fork()");
+        exit(1);
+    }
+    else if(child == 0)
+    {
+        int result = execlp(argv[1], argv[1], argv[2], NULL);
+        perror("execlp");
+        exit(1);
+    }
+    else
+    {
+        waitpid(child, NULL, 0);
+    }
+
+
+    pid_t child_2 = fork();
+    if(child_2 < 0)
+    {
+        perror("Failed to create child process");
+        exit(1);
+    }
+    else if(child_2 == 0)
+    {        
+        int result = execvp("ls", argv + 3);
+        perror("execvp");
+        exit(1);
+    }
+    else
+    {
+        waitpid(child_2, NULL, 0);
+    }
+
+
     // TODO
-    return 0;
+    return exitStatus;
 }
