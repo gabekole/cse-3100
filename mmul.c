f58acdd6c36645104c7ef9709de512276180a975
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2023-11-08 21:32:03 +0000

    attempt multithreaded multiplication

---
+++ mmul.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "matrix.h"
+
+// Search TODO to find the locations where code needs to be completed
+
+#define     NUM_THREADS     2
+
+typedef struct {
+    unsigned int id;
+    TMatrix *m, *n, *t;
+} thread_arg_t;
+
+static void * thread_main(void * p_arg)
+{
+    // TODO
+    thread_arg_t *args = (thread_arg_t *) p_arg;
+
+    for(int i = args->id; i < args->m->nrows; i+= NUM_THREADS)
+    {
+        for(int j = 0; j < args->n->ncols; j++)
+        {
+            for(int k = 0; k < args->n->nrows; k++)
+            {
+                args->t->data[i][j] += args->m->data[i][k] * args->n->data[k][j];
+            }
+        }
+    }
+
+    return NULL;
+}
+
+/* Return the sum of two matrices.
+ *
+ * If any pthread function fails, report error and exit. 
+ * Return NULL if anything else is wrong.
+ *
+ * Similar to mulMatrix, but with multi-threading.
+ */
+TMatrix * mulMatrix_thread(TMatrix *m, TMatrix *n)
+{
+    if (    m == NULL || n == NULL
+         || m->ncols != n->nrows )
+        return NULL;
+
+    TMatrix * t = newMatrix(m->nrows, n->ncols);
+    if (t == NULL)
+        return t;
+
+    // TODO
+    pthread_t thread1;
+    pthread_t thread2;
+
+    thread_arg_t args1 = {0, m, n, t};
+    thread_arg_t args2 = {1, m, n, t};
+
+    pthread_create(&thread1, NULL, &thread_main, &args1);
+    pthread_create(&thread2, NULL, &thread_main, &args2);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    return t;
+}
